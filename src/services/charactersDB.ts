c1f93952c42d22776a2f073e1068594e7ba084f3
AdamDCosta <adam.dcosta@googlemail.com>
2022-01-03 11:17:48 +0000

    fetch function now returns all pages of parameter query

--- src/services/charactersDB.ts
+++ src/services/charactersDB.ts
@@ -1,25 +1,30 @@
 import { Results } from "@/types/rickTypes.interface";
 
 export const fetchRickData = async (): Promise<Results> => {
-  
   const ricksURL = `https://rickandmortyapi.com/api/character?name=rick`;
 
-  const ricksString = localStorage.getItem("ricksObject")
+  const ricksString = localStorage.getItem("ricksObject");
   let ricksObject: Results;
 
   if (ricksString) {
-    ricksObject = JSON.parse(ricksString)
+    ricksObject = JSON.parse(ricksString);
   } else {
     const response = await fetch(ricksURL);
     const rickData = await response.json();
-    ricksObject = rickData
-    localStorage.setItem("ricksObject", JSON.stringify(ricksObject))
+    ricksObject = rickData;
+    const totalPages = rickData.info.pages;
+    for (let i = 2; i <= totalPages; i++) {
+      const nextResponse = await fetch(
+        `https://rickandmortyapi.com/api/character?page=${i}&name=rick`
+      );
+      const nextPageOfData = await nextResponse.json();
+      for (const rick of nextPageOfData.results) {
+        ricksObject.results?.push(rick);
+      }
+    }
+
+    localStorage.setItem("ricksObject", JSON.stringify(ricksObject));
   }
 
   return ricksObject;
 };
-
-// if (rickData.info.next !== null)
-// fetch rickData.info.next
-// const nextPageOfResults
-// ricksObject.results.push()
